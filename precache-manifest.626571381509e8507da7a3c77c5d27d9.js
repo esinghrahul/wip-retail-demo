cbc5f65defdc4df75e72ebd94ae465e08c51395c
esinghrahul <e.singh.rahul@outlook.com>
2020-07-12 18:31:18 +0530

    Updates

---
+++ precache-manifest.626571381509e8507da7a3c77c5d27d9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88c584568c8df189c592507be95719c",
+    "url": "/wip-retail-demo/index.html"
+  },
+  {
+    "revision": "4751059b49cc206d4eca",
+    "url": "/wip-retail-demo/static/css/2.9826cacc.chunk.css"
+  },
+  {
+    "revision": "daa1dfcf51ec9b4bc63f",
+    "url": "/wip-retail-demo/static/css/main.e7b3e96e.chunk.css"
+  },
+  {
+    "revision": "4751059b49cc206d4eca",
+    "url": "/wip-retail-demo/static/js/2.6e14101d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wip-retail-demo/static/js/2.6e14101d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa1dfcf51ec9b4bc63f",
+    "url": "/wip-retail-demo/static/js/main.47533a22.chunk.js"
+  },
+  {
+    "revision": "d917dccdf4cdd5ee6b6e",
+    "url": "/wip-retail-demo/static/js/runtime-main.9f3132ed.js"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/wip-retail-demo/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/wip-retail-demo/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/wip-retail-demo/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/wip-retail-demo/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/wip-retail-demo/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/wip-retail-demo/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/wip-retail-demo/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/wip-retail-demo/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/wip-retail-demo/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/wip-retail-demo/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/wip-retail-demo/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/wip-retail-demo/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/wip-retail-demo/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/wip-retail-demo/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/wip-retail-demo/static/media/fa-solid-900.f3a7d3b5.woff"
+  }
+]);
